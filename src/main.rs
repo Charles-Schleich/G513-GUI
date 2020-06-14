2aed7879b76dade180ea953521f7a3681d6ae380
Charles Schleich <schleich.cdaniel@gmail.com>
2020-06-14 17:35:31 +0200

    Clicking buttons now changes keyboard key to white, added sliders for RGB which do nothing

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ pub use self::main_view::*;
 mod main_state;
 mod main_view;
 
+const WIDTH: f64 = 1000.0; 
+const HEIGHT: f64 = 700.0; 
+
 fn main() {
     Application::from_name("G513 Keyboard")
         .window(move |ctx| {
             Window::create()
                 .title("G513 Keyboard")
                 .position((100.0, 100.0))
-                .size(1000.0, 700.0)
+                .size(WIDTH, HEIGHT)
                 .resizeable(true)
                 .child(MainView::create().title("Hello OrbTk").build(ctx))
                 .build(ctx)
--- src/main_view.rs
+++ src/main_view.rs
@@ -1,5 +1,6 @@
 use orbtk::prelude::*;
 
+use std::process::Command;
 use crate::MainState;
 
 widget!(
@@ -10,31 +11,30 @@ widget!(
 
 const _HEIGHT:f64 = 500.0;
 const _WIDTH:f64 = 1000.0;
+// const _FIRST_ROW: [ (&str,i8) ; 14 ]  = [ ("~",0) ,("1",1) ,("2",2) ,("3",3) ,("4",4) ,("5",5) ,("6",6) ,("7",7) ,("8",8) ,("9",9) ,("0",10) ,("-",11) ,("=",12) ,("BKSPC",13) ];
+
 
 
 impl Template for MainView {
     fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
 
         let mainview = self.name("MainView");
+
+        let colSel = make_colour_select(ctx);
+
         let topgrid =  Grid::create()
                                     .columns(Columns::create()
                                                     .column(500.0)
                                                     .column(500.0)
                                                     .build())
-                                    .child(TextBlock::create()
-                                                        .text("Top Left")
-                                                        .element("text-block")
-                                                        .class("h3")
-                                                        .attach(Grid::column(0))
-                                                        .build(ctx))
+                                    .child(colSel.attach(Grid::column(1))
+                                                 .build(ctx))
                                     .child(TextBox::create()
                                                         .text("Top Right")
                                                         .element("text-block")
                                                         .class("h3")
-                                                        .attach(Grid::column(1))
+                                                        .attach(Grid::column(0))
                                                         .build(ctx));
-
-
         let botgrid = make_keyboard_layout(ctx);
 
         let maingrid = Grid::create()
@@ -43,15 +43,94 @@ impl Template for MainView {
                                                     .row(500.0)
                                                     .build())
                                         .child(topgrid.attach(Grid::row(0))
-                                                    .build(ctx))
+                                                      .build(ctx))
                                         .child(botgrid.attach(Grid::row(1))
-                                                    .build(ctx))
+                                                      .build(ctx))
                                         .build(ctx);
         mainview.child(maingrid)
-
     }
 }
 
+
+
+//    _____      _                     _____      _           _   
+//   / ____|    | |                   / ____|    | |         | |  
+//  | |     ___ | | ___  _   _ _ __  | (___   ___| | ___  ___| |_ 
+//  | |    / _ \| |/ _ \| | | | '__|  \___ \ / _ \ |/ _ \/ __| __|
+//  | |___| (_) | | (_) | |_| | |     ____) |  __/ |  __/ (__| |_ 
+//   \_____\___/|_|\___/ \__,_|_|    |_____/ \___|_|\___|\___|\__|
+
+
+fn make_colour_select(ctx: &mut BuildContext) -> Grid {
+    let local_height = 200.0;
+    
+    let red =  make_colour_slider(ctx, String::from("Red"));
+    let green =  make_colour_slider(ctx, String::from("Green"));
+    let blue =  make_colour_slider(ctx, String::from("Blue"));
+
+    let col_sel =  Grid::create()
+                        .rows(Rows::create()
+                                        .row(local_height/3.0)
+                                        .row(local_height/3.0)
+                                        .row(local_height/3.0)
+                                        .build())
+                        .child(red.attach(Grid::row(0))
+                                  .build(ctx))
+                        .child(green.attach(Grid::row(1))
+                                    .build(ctx))
+                        .child(blue.attach(Grid::row(2))
+                                   .build(ctx));
+    
+
+    col_sel
+}
+
+
+fn make_colour_slider(ctx: &mut BuildContext, colour:String) -> Grid {
+    let local_width = _WIDTH/2.0;
+    let mut col_val_id = colour.clone();
+    col_val_id.push_str("_val");
+    let colour_slider =  Grid::create()
+                        .columns(Columns::create()
+                                        .column(local_width/3.0)
+                                        .column(local_width/3.0)
+                                        .column(local_width/3.0)
+                                        .build())
+                        .child(TextBlock::create()
+                                         .text(colour)
+                                         .class("h3")
+                                         .attach(Grid::column(0))
+                                         .attach(Grid::row(0))
+                                         .build(ctx))
+                        .child(Slider::create()
+                                        .attach(Grid::column(1))
+                                        .attach(Grid::row(0))
+                                        .minimum(0)
+                                        .minimum(255)
+                                        .on_changed(move |states, entity| {
+                                            // let val =((*ctx.get_widget(entity).get::<f64>("val")).floor() as i32).to_string();
+                                        })
+                                        .build(ctx))
+                        .child(TextBlock::create()
+                                         .text("0")
+                                         .id(col_val_id)
+                                         .class("h3")
+                                         .attach(Grid::column(2))
+                                         .build(ctx));
+    colour_slider
+}
+
+
+
+
+//   __  __       _         _  __          _                         _ 
+//  |  \/  |     (_)       | |/ /         | |                       | |
+//  | \  / | __ _ _ _ __   | ' / ___ _   _| |__   ___   __ _ _ __ __| |
+//  | |\/| |/ _` | | '_ \  |  < / _ \ | | | '_ \ / _ \ / _` | '__/ _` |
+//  | |  | | (_| | | | | | | . \  __/ |_| | |_) | (_) | (_| | | | (_| |
+//  |_|  |_|\__,_|_|_| |_| |_|\_\___|\__, |_.__/ \___/ \__,_|_|  \__,_|
+//                                    __/ |                            
+//                                   |___/                
 
 fn make_keyboard_layout(ctx: &mut BuildContext) -> Grid{
 
@@ -101,6 +180,7 @@ fn make_keyboard_layout(ctx: &mut BuildContext) -> Grid{
 }
 
 fn make_f_keys(ctx: &mut BuildContext) -> Entity{
+    let kw = 48.0 ; //  Standard Key width
     
     let mut fkeys =  Grid::create()
                             .columns(Columns::create()
@@ -122,13 +202,14 @@ fn make_f_keys(ctx: &mut BuildContext) -> Entity{
                                              .column(_WIDTH * 0.0405 ) //F12
                                              .build());
 
-    let esc = generate_character_button(ctx,"ESC".to_string(),0);
+    let esc = generate_character_button(ctx,"ESC".to_string(),0,kw);
     fkeys = fkeys.child(esc);
-    let fkey_letters  = [("F1",2),("F2",3),("F3",4),("F4",5),("F5",7),("F6",8),("F7",9),("F8",10),("F9",12),("F10",13),("F11",14),("F12",15)];
+    let fkey_letters  = [("F1",2,kw),("F2",3,kw),("F3",4,kw),("F4",5,kw),("F5",7,kw),("F6",8,kw),("F7",9,kw),("F8",10,kw),("F9",12,kw),("F10",13,kw),("F11",14,kw),("F12",15,kw)];
+
     for i in fkey_letters.iter(){
-        let (key,pos) = i;
+        let (key,pos,width ) = i;
         // let fkey = generate_text(ctx,key.to_string(),*pos as usize);
-        let fkey = generate_character_button(ctx, key.to_string(),*pos as usize);
+        let fkey = generate_character_button(ctx, key.to_string(),*pos as usize, *width);
         fkeys = fkeys.child(fkey);
     }
     return fkeys.build(ctx);
@@ -138,6 +219,7 @@ fn make_main_keys(ctx: &mut BuildContext) -> Entity{
     let key_w = _WIDTH * 0.0405;
     let key_h = _HEIGHT * 0.1221;
     let mainboard_HEIGHT = _HEIGHT*0.7175;
+    let kw = 48.0 ; //  Standard Key width
 
     let mut mainboard = Grid::create()
                             .rows(Rows::create()
@@ -148,24 +230,26 @@ fn make_main_keys(ctx: &mut BuildContext) -> Entity{
                                         .row(mainboard_HEIGHT * 0.2) 
                                         .build());
 
-    // First Row 
-    let first_row_keys: Vec<(&str,i32)>  = vec![("~",0) ,("1",1) ,("2",2) ,("3",3) ,("4",4) ,("5",5) ,("6",6) ,("7",7) ,("8",8) ,("9",9) ,("0",10) ,("-",11) ,("=",12) ,("BKSPC",13) ];
+    // First Row
+    
+    let first_row_keys: Vec<(&str,i32,f64)>  = vec![("~",0,kw) ,("1",1,kw) ,("2",2,kw) ,("3",3,kw) ,("4",4,kw) ,("5",5,kw) ,("6",6,kw) ,("7",7,kw) ,("8",8,kw) ,("9",9,kw) ,("0",10,kw) ,("-",11,kw) ,("=",12,kw) ,("BKSPC",13,96.0) ];
     let first_row= make_row(ctx, first_row_keys);
 
-    // Second Row
-    let second_row_keys: Vec<(&str,i32)> = vec![("TAB",0) ,("Q",1) ,("W",2) ,("E",3) ,("R",4) ,("T",5) ,("Y",6) ,("U",7) ,("I",8) ,("O",9) ,("P",10) ,("[",11) ,("]",12) ,("ENTER",13) ];
+    // // Second Row
+    let second_row_keys: Vec<(&str,i32,f64)>  = vec![("TAB",0,66.66) ,("Q",1,kw) ,("W",2,kw) ,("E",3,kw) ,("R",4,kw) ,("T",5,kw) ,("Y",6,kw) ,("U",7,kw) ,("I",8,kw) ,("O",9,kw) ,("P",10,kw) ,("[",11,kw) ,("]",12,kw) ,("ENTER",13,66.66) ];
     let second_row= make_row(ctx, second_row_keys);
 
-    // third Row
-    let third_row_keys: Vec<(&str,i32)> = vec![("CAPS",0) ,("A",1) ,("S",2) ,("D",3) ,("F",4) ,("G",5) ,("H",6) ,("J",7) ,("K",8) ,("L",9) ,(":",10) ,("\"",11) ,("\\",12) ];
+    // // third Row
+    let third_row_keys: Vec<(&str,i32,f64)>  = vec![("CAPS",0,80.0) ,("A",1,kw) ,("S",2,kw) ,("D",3,kw) ,("F",4,kw) ,("G",5,kw) ,("H",6,kw) ,("J",7,kw) ,("K",8,kw) ,("L",9,kw) ,(":",10,kw) ,("\"",11,kw) ,("\\",12,kw) ];
     let third_row= make_row(ctx, third_row_keys);
 
-    // third Row
-    let fourth_row_keys: Vec<(&str,i32)> = vec![("SHIFT",0) ,("Z",1) ,("X",2) ,("C",3) ,("V",4) ,("B",5) ,("N",6 ) ,("M",7 ) ,("<",8 ) ,(">",9 ) ,("?",10 ) ,("SHIFT",11 ) ];
+    // // Fourth Row
+    let fourth_row_keys: Vec<(&str,i32,f64)>  = vec![("SHIFT",0,104.0) ,("Z",1,kw) ,("X",2,kw) ,("C",3,kw) ,("V",4,kw) ,("B",5,kw) ,("N",6 ,kw) ,("M",7 ,kw) ,("<",8 ,kw) ,(">",9 ,kw) ,("?",10 ,kw) ,("SHIFT",11 ,130.67) ];
     let fourth_row= make_row(ctx, fourth_row_keys);
 
-    // third Row
-    let fifth_row_keys: Vec<(&str,i32)> = vec![("CTRL",0) ,("WIN",1) ,("ALT",2) ,("SPACE",3) ,("ALTGR",4) ,("FN",5) ,("OPT",6 ) ,("CTRL",7 ) ,];
+    // // Fifth Row
+    let bk = 56.0;
+    let fifth_row_keys: Vec<(&str,i32,f64)>  = vec![("LCTRL",0,66.66) ,("WIN",1,bk) ,("ALT",2,bk) ,("SPACE",3,312.0) ,("ALTGR",4,bk) ,("FN",5,bk) ,("OPT",6 ,bk) ,("RCTRL",7 ,66.66)];
     let fifth_row= make_row(ctx, fifth_row_keys);
 
     mainboard = mainboard.child(Container::create()
@@ -192,43 +276,37 @@ fn make_main_keys(ctx: &mut BuildContext) -> Entity{
     return mainboard.build(ctx);
 }
 
-
-
-fn make_row(ctx: &mut BuildContext, row_keys: Vec<(&str,i32)> ) -> Grid {
+fn make_row(ctx: &mut BuildContext, row_keys: Vec<(&str,i32,f64)> ) -> Grid {
     let key_w = _WIDTH * 0.0405;
     let mut row_columns= Columns::create();
     for _ in 0..row_keys.len(){ row_columns=row_columns.column(key_w); }
 
-    let mut row =  Grid::create().columns(row_columns.build());
+    let mut row =  Grid::create().columns(row_columns.build()).clip(false);
     for i in row_keys.iter(){
-        let (key,pos) = i;
-        let fkey = generate_character_button(ctx,key.to_string(),*pos as usize);
+        let (key,pos,width) = i;
+        let fkey = generate_character_button(ctx,key.to_string(),*pos as usize, *width);
         row = row.child(fkey);
     }
     row
 }
 
-fn generate_text(ctx: &mut BuildContext, name: String, pos: usize) -> Entity {
-    TextBlock::create().text(name)
-                       .element("text-block")
-                       .class("h4")
-                       .attach(Grid::column(pos))
-                       .build(ctx)
-}
-
-
 fn generate_character_button(
     ctx: &mut BuildContext,
     key: String,
     column: usize,
+    width : f64,
 ) -> Entity {
     let mut button = Button::create()
         .class("single_content")
-        .min_size(48.0, 48.0)
+        .min_size(width, 48.0)
         .text(key.clone())
-        .on_click(move |states, _| -> bool {
+        .element("button")
+        .margin((0.0, 8.0, 0.0, 0.0))
+        .padding(20)
+        // .vertical_alignment()
+        .on_click(move | states, _| -> bool {
             print!("{}",key);
-
+            change_key_colour(&key);
             // state(id, states).action(Action::Character(sight));
             true
         })
@@ -240,3 +318,39 @@ fn generate_character_button(
     }
     button.build(ctx)
 }
+
+fn change_key_colour(key :&str){
+    let white = "ffffff";
+    // ./g810-led -dv 046d -dp c33c -tuk 1 -k '+key+' '+colour
+
+    let _key = match key {
+        "BKSPC"=> "f", 
+        "LCTRL" => "ctrll",
+        "RCTRL" => "ctrlright",
+        "ALT" => "altright",
+        "LSHIFT" => "shiftleft",
+        "RSHIFT" => "shiftright",
+        "CAPS" => "capslock",
+        ":"=>"semicolon",
+        "\'"=>"quote",
+        "WIN" => "winleft",
+        n => {n}
+    };
+
+    let mut exitStatus = Command::new("sudo")
+                                    .arg("./g810-led")
+                                    .arg("-dv")
+                                    .arg("046d")
+                                    .arg("-dp")
+                                    .arg("c33c")
+                                    .arg("-tuk")
+                                    .arg("-1")
+                                    .arg("-k")
+                                    .arg(_key)
+                                    .arg(white)
+                                    .status()
+                                    .expect("process failed to execute");
+    println!("{}",exitStatus);
+
+}
+
